b010cf08490daf4a6d5641b31c0a88269c3af9f0
Marty Kraimer <mrkraimer@comcast.net>
2014-07-02 07:11:50 -0400

    add some records for testing easyPVAJava

--- exampleDatabase/iocBoot/exampleDatabase/st.cmd
+++ exampleDatabase/iocBoot/exampleDatabase/st.cmd
@@ -7,12 +7,12 @@ dbLoadDatabase("dbd/exampleDatabase.dbd")
 exampleDatabase_registerRecordDeviceDriver(pdbbase)
 
 ## Load record instances
-dbLoadRecords("db/dbScalar.db","name=double00,type=ao")
-dbLoadRecords("db/dbScalar.db","name=double01,type=ao")
-dbLoadRecords("db/dbScalar.db","name=double02,type=ao")
-dbLoadRecords("db/dbScalar.db","name=double03,type=ao")
-dbLoadRecords("db/dbScalar.db","name=double04,type=ao")
-dbLoadRecords("db/dbScalar.db","name=double05,type=ao")
+dbLoadRecords("db/dbDouble.db","name=double00")
+dbLoadRecords("db/dbDouble.db","name=double01")
+dbLoadRecords("db/dbDouble.db","name=double02")
+dbLoadRecords("db/dbDouble.db","name=double03")
+dbLoadRecords("db/dbDouble.db","name=double04")
+dbLoadRecords("db/dbDouble.db","name=double05")
 dbLoadRecords("db/dbStringArray.db","name=stringArray01")
 dbLoadRecords("db/dbEnum.db","name=enum01")
 dbLoadRecords("db/dbCounter.db","name=counter01");
